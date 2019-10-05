f51cbc3273903f38dd76e730fc3bc85b75fd8e0b
Gensen Choy <gensen.choy@gmail.com>
2019-10-05 15:07:33 -0400

    pankot

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import { DateTime } from 'luxon';
+import 'normalize.css';
+import React, { PureComponent } from 'react';
+import { v4 } from 'uuid';
 import './App.css';
+import { Pankot } from './components/Pankot/Pankot';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface State {
+  uuid: string;
+}
+export class App extends PureComponent<{}, State> {
+  state: State = {
+    uuid: v4(),
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Pankot text={this.state.uuid} code="KeyQ" />
+        <Pankot text={DateTime.utc().toISO()} code="KeyW" />
+      </div>
+    );
+  }
 }
 
 export default App;
